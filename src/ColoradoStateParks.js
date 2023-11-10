cc1afd9cabab4c73cc2f4f42e036f7ab93b04c15
Brenda <brendakungu3@gmail.com>
2023-11-10 21:53:55 +0300

    lab done

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -6,3 +6,22 @@ function ColoradoStateParks() {
 
   return <h1>Colorado State Parks!</h1>;
 }
+
+
+function Tweet(){
+  const currentTime = new Date().toString();
+
+  //this returns JSX
+  return(
+    <div classname="tweet">
+      <img src="http://twitter.com/some-avatar.png" classname="tweet_avatar"/>
+      <div classname="tweet_body">
+        <p>I am a good programmer.</p>
+        <p>{Math.floor(Math.random()*100)}retweets</p>
+        <p>{currentTime}</p>
+      </div>
+    </div>
+  )
+}
+
+
